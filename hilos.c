f9b192da7b81ef5e700180ee01670b8ef8930e7c
jamamanica <jamamanica@unjbg.edu.pe>
2026-09-23 10:13:53 -0500

    Analisis de rendimiento

--- hilos.c
+++ hilos.c
@@ -1,27 +1,50 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
 
 pthread_mutex_t mutex;
 
-void* hilo_funcion_con_mutex(void* arg){
-	pthread_mutex_lock(&mutex);
-	printf("Hola desde el hilo %ld\n",(long)arg);
-	pthread_mutex_unlock(&mutex);
-	pthread_exit(NULL);
+void* hilo_funcion_con_mutex(void* arg) {
+    pthread_mutex_lock(&mutex);
+    printf("Hola desde el hilo %ld\n", (long)arg);
+    pthread_mutex_unlock(&mutex);
+    pthread_exit(NULL);
 }
 
-int main(){
-	pthread_t hilo1, hilo2;
-	pthread_mutex_init(&mutex,NULL);
+void ejecutar_con_hilos(int num_hilos) {
+    pthread_t hilos[num_hilos];
+    pthread_mutex_init(&mutex, NULL);
 
-	pthread_create(&hilo1,NULL,hilo_funcion_con_mutex,(void*)1);
-	pthread_create(&hilo2,NULL,hilo_funcion_con_mutex,(void*)2);
+    clock_t start, end;
+    start = clock();
 
-	pthread_join(hilo1,NULL);
-	pthread_join(hilo2,NULL);
+    for (long i = 0; i < num_hilos; i++) {
+        pthread_create(&hilos[i], NULL, hilo_funcion_con_mutex, (void*)i);
+    }
 
-	pthread_mutex_destroy(&mutex);
-	printf("Finalizo la ejecucion del programa principal\n");
-	return 0;
+    for (int i = 0; i < num_hilos; i++) {
+        pthread_join(hilos[i], NULL);
+    }
+
+    end = clock();
+
+    pthread_mutex_destroy(&mutex);
+
+    double cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Tiempo total de ejecucion con %d hilos: %f segundos\n", num_hilos, cpu_time_used);
+}
+
+int main() {
+    printf("Ejecutando con 1 hilo...\n");
+    ejecutar_con_hilos(1);
+
+    printf("Ejecutando con 5 hilos...\n");
+    ejecutar_con_hilos(5);
+
+    printf("Ejecutando con 10 hilos...\n");
+    ejecutar_con_hilos(10);
+
+    printf("Finalizo la ejecucion del programa principal\n");
+    return 0;
 }
